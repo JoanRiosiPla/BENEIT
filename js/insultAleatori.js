db983f95be47654df1902d1229b9d0c800e1aa3e
Joan Rios i Pla <joanriosipla@gmail.com>
2023-11-18 16:13:05 +0100

    separat part del js

---
+++ js/insultAleatori.js
@@ -0,0 +1,30 @@
+// Joan Rios i Pla - 2023 - _joanrios - contact.joanrios@gmail.com
+
+// Get the domain
+const domain = location.protocol + '//' + location.host + location.pathname;
+
+// Get the search query
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+query = urlParams.get('search');
+
+// Get elements
+const searchBar = document.getElementById("searchBar");
+const searchX = document.getElementById("search-x");
+
+// Show X if there is a query
+if (query) {
+    searchX.classList.remove("hidden");
+}
+
+var data;
+// Get insults.json
+fetch('./insults.json?nocache=' + (new Date()).getTime())
+.then(response => response.json())
+.then(jsonData => {
+    console.log('Fetched insults.json');
+    data = jsonData; 
+    // Insult ràndom
+    const insult = data.insults[Math.floor(Math.random() * data.insults.length)].paraula;
+    document.getElementById("insultAleatori").innerHTML = insult;
+});
